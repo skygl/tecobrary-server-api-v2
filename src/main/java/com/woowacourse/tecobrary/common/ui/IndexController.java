8117f6e162cbea973026999a3fb6abeeb0cdf584
thedevluffy <thedevluffy@gmail.com>
2019-09-18 00:02:24 +0900

    feat: IndexController 를 수정한다.

--- src/main/java/com/woowacourse/tecobrary/common/ui/IndexController.java
+++ src/main/java/com/woowacourse/tecobrary/common/ui/IndexController.java
@@ -5,11 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.extern.slf4j.Slf4j;
-
 @RestController
 @RequestMapping("/api/v1")
-@Slf4j
 public class IndexController {
 
     @GetMapping
